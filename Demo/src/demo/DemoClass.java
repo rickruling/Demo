aa960028000cbc61f989edc35e361af2ac7df2a2
Vishnu <rickruling@yahoo.comgit config --global user.name Vishnugit config --global user.email rickruling@yahoo.com>
2015-10-14 22:55:47 +0530

    addition added

--- Demo/src/demo/DemoClass.java
+++ Demo/src/demo/DemoClass.java
@@ -1,11 +1,17 @@
 package demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class DemoClass {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hi");
-
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter two numbers");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = br.read();
+		int b = br.read();
+		System.out.println("The sum is "+(a+b));
 	}
 
 }
