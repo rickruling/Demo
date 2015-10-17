62f0abbd15a01b2d915b319e2ec561648ee2396b
Vishnu <rickruling@yahoo.comgit config --global user.name Vishnugit config --global user.email rickruling@yahoo.com>
2015-10-17 10:29:37 +0530

    while loop added

--- Demo/src/demo/DemoClass.java
+++ Demo/src/demo/DemoClass.java
@@ -5,13 +5,17 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DemoClass {
-
 	public static void main(String[] args) throws IOException {
+
+	String ch;
+		do{
 		System.out.print("Enter two numbers : ");
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int a = Integer.parseInt(br.readLine());
 		int b = Integer.parseInt(br.readLine());
-		System.out.println("The sum is "+(a+b));
+		System.out.println("The sum is " + (a + b));
+		System.out.print("want another addition:");
+		ch = br.readLine();
+		}while(ch.equalsIgnoreCase("y".trim()));
 	}
-
 }
